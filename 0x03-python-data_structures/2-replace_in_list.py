155d0410d561f999409c6acc59d3d72ea9eebce7
henriettetuombe <116841112+henriettetuombe@users.noreply.github.com>
2023-07-15 08:47:38 +0200

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 2-replace_in_list.py
-# Henriette Tuombe <tuombehenriette@gmail.com.com>
+# Henriette Tuombe <tuombehenriette@gmail.com>
 
 
 def replace_in_list(my_list, idx, element):
