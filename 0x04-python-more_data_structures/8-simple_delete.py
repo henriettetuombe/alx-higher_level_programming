eea54e6defe4a7c2cc3accc9945c2ea073998979
henriettetuombe <116841112+henriettetuombe@users.noreply.github.com>
2023-07-19 14:44:59 +0200

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# 8-simple_delete.py
+# Henriette Tuombe
 
 
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
         del a_dictionary[key]
-    return (a_dictionary) 
+    return (a_dictionary)
