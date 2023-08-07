74747132cb3f7d8a47d21ae6760ad6198ea553fc
henriettetuombe <116841112+henriettetuombe@users.noreply.github.com>
2023-08-07 12:43:46 +0000

    python more classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Henriette Tuombe <tuombehenriette@gmail.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
